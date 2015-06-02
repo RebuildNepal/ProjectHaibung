51e97a394fb50285dc7997a31cad6e5d7fd78f81
sura8257 <thapa.suraj751@gmail.com>
2015-06-02 22:49:57 +1000

    sthapa - updated image file extension.

--- protected/views/layouts/slider.php
+++ protected/views/layouts/slider.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="slider-wrapper">
         <div id="camera01" class="camera_wrap">
-            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child3.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung1.jpeg">
+            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child3.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung1.jpg">
             </div>
-            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child2.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung2.jpeg">
+            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child2.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung2.jpg">
             </div>
-            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung3.jpeg">
-            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung4.jpeg">
+            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung3.jpg">
+            <div data-src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/child.jpeg" data-thumb="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/home_haibung4.jpg">
             </div>
         </div>
     </div>
