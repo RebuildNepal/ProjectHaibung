43266ac22db052982c7104db702a70f1b1f5c7a5
sura8257 <thapa.suraj751@gmail.com>
2015-06-03 16:35:06 +1000

    sthapa - updated summary and removed search.

--- protected/views/site/summary.php
+++ protected/views/site/summary.php
@@ -1,31 +1,25 @@
 <div class="grid_12  wow fadeInUp" data-wow-delay="0.2s" style="margin: 20px 0">
         <h2>Project Haibung</h2>
         <h3>
-            <a href="#">Lorem ipsum dolor sit amet, consec teer adipiscing. Prsent vestibulum.</a>
+            <a href="#">More about ProjectHaibung</a>
         </h3>
-        <p>Lorem ipsum dolor sit amet, conc tetur adipi scing. Praesent vestibuum molstie lacuiirhs. Aenean
-            non ummy dreriauris. Phasellllus. porta. Fusce suscipit variusium sociis totdnatibus et magis
-            dis parturient montes, nasceidiculus mus. Nulla dui. Fusce feugiatle. suada odio. Morbi nunc
-            odio, gravida at.</p>
-        
-        <p>Lorem ipsum dolor sit amet, conc tetur adipi scing. Praesent vestibuum molstie lacuiirhs. Aenean
-            non ummy dreriauris. Phasellllus. porta. Fusce suscipit variusium sociis totdnatibus et magis
-            dis parturient montes, nasceidiculus mus. Nulla dui. Fusce feugiatle. suada odio. Morbi nunc
-            odio, gravida at.</p>
-        
-        <p>Lorem ipsum dolor sit amet, conc tetur adipi scing. Praesent vestibuum molstie lacuiirhs. Aenean
-            non ummy dreriauris. Phasellllus. porta. Fusce suscipit variusium sociis totdnatibus et magis
-            dis parturient montes, nasceidiculus mus. Nulla dui. Fusce feugiatle. suada odio. Morbi nunc
-            odio, gravida at.</p>
-        
-        <p>Lorem ipsum dolor sit amet, conc tetur adipi scing. Praesent vestibuum molstie lacuiirhs. Aenean
-            non ummy dreriauris. Phasellllus. porta. Fusce suscipit variusium sociis totdnatibus et magis
-            dis parturient montes, nasceidiculus mus. Nulla dui. Fusce feugiatle. suada odio. Morbi nunc
-            odio, gravida at.</p>
-        
-        <p>Lorem ipsum dolor sit amet, conc tetur adipi scing. Praesent vestibuum molstie lacuiirhs. Aenean
-            non ummy dreriauris. Phasellllus. porta. Fusce suscipit variusium sociis totdnatibus et magis
-            dis parturient montes, nasceidiculus mus. Nulla dui. Fusce feugiatle. suada odio. Morbi nunc
-            odio, gravida at.</p>
+        <p>As the destruction unraveled before us, the meaning of our lives and priorities also took a
+         new direction. We want Nepal to rise, and rise BETTER & HIGHER this time and want to contribute
+         on an individual level to the nation building process.</p>
+
+         <p>We chose not to wait for the government or INGOs/NGOs to come and rebuild schools in Haibung, because
+         it might take a good couple of years. This delay means children dropping out of school permanently
+         and going to cities for work in a restaurant or as a domestic helper or even as a child labor in a brick
+         or garment industries.</p>
+
+        <p>Project Haibung is an initiative by team of likeminded Nepalese youth who is committed to using 100% of the
+         funds it raises directly to rebuild schools for the benefit of children in Haibung, Nepal.
+         We are also committed to full transparency and accountability of funds raised and distributed,
+         which we will post publicly on our website and on social media. All of Project Haibung team members
+         are voluntarily working for this cause, which means that there are no overhead costs.</p>
+
+        <p>Please donate generously and spread the word. We will continue to provide updates and information regarding
+        the allocation of funds on our website, and on our Facebook and Twitter pages. Thank you
+        all for your support.</p>
     </div>
 </div>
