52df9c041ce52ce7e93ec8a25247d98c0aa6f6a4
sura8257 <thapa.suraj751@gmail.com>
2015-06-05 13:49:20 +1000

    sthapa - updated footer size.

--- protected/views/layouts/footer.php
+++ protected/views/layouts/footer.php
@@ -7,14 +7,12 @@
             <span id="copyright-year"></span> &copy;
             <a href="index-5.html">Privacy Policy</a>
             Project Haibung
-        </p>
+            </p>
+        <p>projecthaibung@gmail.com</p>
         <ul class="list">
             <li>
-                <p>Email: projecthaibung@gmail.com</p>
-            </li>
-            <li>
              <a href="<?php echo Yii::app()->createUrl('/');?>">
-             <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/rackspace.jpg" alt="" width="200px" height="100px"/>
+             <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/rackspace.jpg" alt="" width="200px" height="70px"/>
              </a>
             </li>
         </ul>
