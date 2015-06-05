10b05b7fdb7314584fc3230e81c77fef1396cf5a
sura8257 <thapa.suraj751@gmail.com>
2015-06-05 13:27:50 +1000

    sthapa - added footer logo.

--- protected/views/layouts/footer.php
+++ protected/views/layouts/footer.php
@@ -12,6 +12,11 @@
             <li>
                 <p>Email: projecthaibung@gmail.com</p>
             </li>
+            <li>
+             <a href="<?php echo Yii::app()->createUrl('/');?>">
+             <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/rackspace.jpg" alt="" width="300px" height="200px"/>
+             </a>
+            </li>
         </ul>
     </div>
 </footer>
