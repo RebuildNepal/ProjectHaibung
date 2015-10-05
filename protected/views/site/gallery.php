2ce6c79c8033fe46bdc650e6e52f1c93273589f9
Girish Lamichhane <girish.l@hotmail.com>
2015-10-05 16:17:50 +1100

    gallery update

--- protected/views/site/gallery.php
+++ protected/views/site/gallery.php
@@ -1,72 +1,146 @@
 
-<div class="wrapper2">
-            <div id="filters" class="button-group  wow fadeInRight" data-wow-delay="0.3s">
-                <a class="button active" data-filter="*" href="#">All</a>
-            </div>
-            </div>
-            <div class="clearfix"></div>
-            <div class="isotope">
-                <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.3s"
-                    <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img7_original.jpg">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img7.jpg" alt=""/>
-                        <span class="overlay"></span>
-                        <span class="hover">zoom</span>
-                    </a>
-                    <h3>
-                        <a href="#">Haibung Mahadevstan Secondary School</a>
-                    </h3>
-                    <p class="off1">400 students from class 1 to 10 used to study in this school.
-                    </p>
-                     data-category="praesent">
-                </div>
-                <div class="element-item grid_4 vestibulum  wow fadeInRight" data-wow-delay="0.2s"
-                    <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img8_original.jpg">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img8.jpg" alt=""/>
-                        <span class="overlay"></span>
-                        <span class="hover">zoom</span>
-                    </a>
-                    <h3>
-                        <a href="#">Haibung Mahadevstan Secondary School</a>
-                    </h3>
-                    <p class="off1">Block 3 of Haibung Mahadevstan Secondary School</p>
-                     data-category="vestibulum">
-                </div>
-                <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.1s"
-                    <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img9_original.jpg">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img9.jpg" alt=""/>
-                        <span class="overlay"></span>
-                        <span class="hover">zoom</span>
-                    </a>
-                    <h3>
-                        <a href="#">School 4: Haibung Mahadevstan Secondary School</a>
-                    </h3>
-                    <p class="off1">Block 4, locals clearing out rubble</p>
-                     data-category="vestibulum">
-                </div>
-                <div class="element-item grid_4 perspiciatis  wow fadeInLeft" data-wow-delay="0.1s"
-                    <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img10_original.jpg">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img10.jpg" alt=""/>
-                        <span class="overlay"></span>
-                        <span class="hover">zoom</span>
-                    </a>
-                    <h3>
-                        <a href="#">Part 1: Goverment Approval Letter</a>
-                    </h3>
-                    <p class="off1">Approval letter to rebuild school</p>
-                     data-category="vestibulum">
-                </div>
+<div class="isotope">
+    <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.3s"
+         data-category="praesent">
+        <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img1_original.jpg">
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img1.jpg" alt=""/>
+            <span class="overlay"></span>
+            <span class="hover">zoom</span>
+        </a>
+        <h3>
+            <a href="#">School 1: Kalika Sharan Lower Secondary School</a>
+        </h3>
+        <p class="off1">120 students from class 1 to 8 used to study in this school.</p>
+    </div>
+    <div class="element-item grid_4 vestibulum  wow fadeInRight" data-wow-delay="0.2s"
+         data-category="vestibulum">
+        <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img2_original.jpg">
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img2.jpg" alt=""/>
+            <span class="overlay"></span>
+            <span class="hover">zoom</span>
+        </a>
+        <h3>
+            <a href="#">School 1: Only blackboard remains.</a>
+        </h3>
+        <p class="off1">Earthquake took everything down except blackboard.</p>
+    </div>
+    <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.1s"
+         data-category="vestibulum">
+        <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img3_original.jpg">
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img3.jpg" alt=""/>
+            <span class="overlay"></span>
+            <span class="hover">zoom</span>
+        </a>
+        <h3>
+            <a href="#">School 1: Kalika Sharan Lower Secondary School</a>
+        </h3>
+        <p class="off1">Remains of class room after the earthquake.</p>
+    </div>
+    <!--<?php /* <div class="element-item grid_4 perspiciatis  wow fadeInLeft" data-wow-delay="0.1s"
+  data-category="vestibulum">
+  <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img4_original.jpg">
+  <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img4.jpg" alt=""/>
+  <span class="overlay"></span>
+  <span class="hover">zoom</span>
+  </a>
+  <h3>
+  <a href="#">School 2: Ananteshwori Primary School, Ward # 4</a>
+  </h3>
+  <p class="off1">45 students from class 1 to 5 used to study in this school.
+  </p>
+  </div>
 
-                <div class="element-item grid_4 praesent  wow fadeInLeft" data-wow-delay="0.2s"
-                    <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img11_original.jpg">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img11.jpg" alt=""/>
-                        <span class="overlay"></span>
-                        <span class="hover">zoom</span>
-                    </a>
-                    <h3>
-                        <a href="#">Part 2: Goverment Approval Letter</a>
-                    </h3>
-                    <p class="off1">Approval letter to rebuild school</p>
-                     data-category="vestibulum">
-                </div>
-            </div>
-        </div>
+  <div class="element-item grid_4 praesent  wow fadeInLeft" data-wow-delay="0.2s"
+  data-category="vestibulum">
+  <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img5_original.jpg">
+  <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img5.jpg" alt=""/>
+  <span class="overlay"></span>
+  <span class="hover">zoom</span>
+  </a>
+  <h3>
+  <a href="#">School 2: Ananteshwori Primary School, Ward #4</a>
+  </h3>
+  <p class="off1">Wall of the school which is about to collaps.
+  </p>
+  </div>
+
+  <div class="element-item grid_4 vestibulum  wow fadeInLeft" data-wow-delay="0.3s"
+  data-category="perspiciatis">
+  <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img6_original.jpg">
+  <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img6.jpg" alt=""/>
+  <span class="overlay"></span>
+  <span class="hover">zoom</span>
+  </a>
+  <h3>
+  <a href="#">School 3: Nimna Madhyamik School, Ward #3</a>
+  </h3>
+  <p class="off1">50 students from class 1 to 3 used to study in this school.
+  </p>
+  </div>
+  <div class="element-item grid_4 perspiciatis  wow fadeInRight" data-wow-delay="0.1s"
+  data-category="school4">
+  <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img7_original.jpg">
+  <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img7.jpg" alt=""/>
+  <span class="overlay"></span>
+  <span class="hover">zoom</span>
+  </a>
+  <h3>
+  <a href="#">School 4: Haibung Mahadustan Secondary School</a>
+  </h3>
+  <p class="off1">400 students from class 1 to 10 used to study in this school.
+  </p>
+  </div>
+
+  <div class="element-item grid_4 perspiciatis  wow fadeInRight" data-wow-delay="0.2s"
+  data-category="school4">
+  <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img8_original.jpg">
+  <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img8.jpg" alt=""/>
+  <span class="overlay"></span>
+  <span class="hover">zoom</span>
+  </a>
+  <h3>
+  <a href="#">School 4: Haibung Mahadustan Secondary School</a>
+  </h3>
+  <p class="off1">Block 3 of Haibung Mahadustan Secondary School</p>
+  </div>
+
+  <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.3s"
+  data-category="school4">
+  <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img9_original.jpg">
+  <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img9.jpg" alt=""/>
+  <span class="overlay"></span>
+  <span class="hover">zoom</span>
+  </a>
+  <h3>
+  <a href="#">School 4: Haibung Mahadustan Secondary School</a>
+  </h3>
+  <p class="off1">Block 4, locals clearing out rubble</p>
+  </div> */ ?> -->
+</div>
+
+<div class="isotope">
+    <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.3s"
+         data-category="All">
+        <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img10_original.jpg">
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img10.jpg" alt=""/>
+            <span class="overlay"></span>
+            <span class="hover">zoom</span>
+        </a>
+        <h3>
+            <a href="#">Part 1: Goverment Approval Letter</a>
+        </h3>
+        <p class="off1">Approval letter to rebuild school</p>
+    </div>
+    <div class="element-item grid_4 praesent  wow fadeInRight" data-wow-delay="0.3s"
+         data-category="All">
+        <a class="gallery" data-gal href="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img11_original.jpg">
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/asset/images/page3-img11.jpg" alt=""/>
+            <span class="overlay"></span>
+            <span class="hover">zoom</span>
+        </a>
+        <h3>
+            <a href="#">Part 2: Goverment Approval Letter</a>
+        </h3>
+        <p class="off1">Approval letter to rebuild school</p>
+    </div>
+</div>
