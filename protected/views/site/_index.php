b9e82bab00baae29f7e4be49caca39a05aed0496
Girish Lamichhane <girish.l@hotmail.com>
2016-12-11 20:32:21 +1100

    update project haibung index page

--- protected/views/site/_index.php
+++ protected/views/site/_index.php
@@ -8,7 +8,8 @@
         <p>Around 720 children currently have no access to education, raising concerns regarding their security and academic future.</p>
         <p>We are a team of Nepalese living in Nepal and abroad, working closely with local youth in Haibung. We are determined to provide these children with a safe and secure learning environment.</p>
         <p>This project does not end with the physical buildings. With your help we will continue to support the children of Haibung by providing mentoring, career counseling and scholarship opportunities.</p>
-        
+        <p>Project Haibung has also partnered with Nepal Ko Yuwa (tax exempt 501c3 registered organisation in the US) under their <a href="http://www.nepalrising.or">Nepal Rising Campaign</a>.This partnership provides Project Haibung platform to raise and collect funds in the US; and establish support and network with other partnering organisation as needed.</p>
+
         <p><a href="https://drive.google.com/file/d/0Bxav3qco08CYcXpQdDc0Qmt3bEk/view" target="_blank" id="view-proposal" style="padding: 10px; margin:10px 0; color: white; font-weight: bold; background-color: #3399FF;">View Our Proposal Here ...</a></p>
         
         <a class="btn" href="<?php echo Yii::app()->createUrl('site/summary');?>">more info</a>
